2f432b904adfe7f4ab0a800d1245633937b007a5
Mandy Chang <68087654+mandy-chang@users.noreply.github.com>
2025-04-16 11:24:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask
+from flask_login import LoginManager
+from models import db, User
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a secure key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+from routes import *
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Creates database tables
+    app.run(debug=True)
